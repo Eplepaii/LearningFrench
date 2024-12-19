6952d9dd7de66d6c49d6c7f1c028c12b1961f7ba
teoti <teoti@DESKTOP-85MNPKM>
2024-12-19 20:44:28 +0100

    Add project files.

---
+++ LearningFrench/FrenchNorwegianDict.cs
@@ -0,0 +1,45 @@
+﻿namespace LearningFrench;
+
+internal static class FrenchNorwegianDict
+{
+    public static Dictionary<string, string> Dictionary = new() {
+        //Verb-ER
+            {"Ranger", "Å rydde"},
+            {"Achetter", "Å kjøpe"},
+            {"Adorer", "Å elske"},
+            {"Aider", "Å hjelpe"},
+            {"Chanter", "Å synge"},
+            {"Commander", "Å bestille"},
+            {"Commencer", "Å begynne"},
+            {"Dancer", "Å danse"},
+            {"Détester", "Å hate"},
+            {"Écouter", "Å lytte"},
+            {"Envoyer", "Å sende"},
+            {"Embrasser", "Å kysse"},
+            {"Habiter", "Å bo"},
+            {"Jouer", "Å spille"},
+            {"Manger", "Å spise"},
+            {"Nager", "Å svømme"},
+            {"Participer", "Å delta"},
+            {"Payer", "Å betale"},
+            {"Préférer", "Å foretrekke"},
+            {"Regarder", "Å se (på)" },
+            {"Rencontrer", "Å møte"},
+            {"Terminer", "Å slutte"},
+            {"Visiter", "Å besøke"},
+
+        //Verb-RE
+            {"Entendre", "Å høre"},
+            {"Perdre", "Å miste"},
+            {"Répondre", "Å svare"},
+            {"Vendre", "Å selge"},
+            {"Rendre", "Å gi (tilbake)"},
+        //Verb-IR
+            {"Atterrir", "Å lande (med fly)" },
+            {"Choisir", "Å velge"},
+            {"Grandir", "Å vokse"},
+            {"Réfléchir", "Å tenke (etter)"},
+            {"Réussir", "Å Lykkes"}
+
+        };
+}
---
+++ LearningFrench/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LearningFrench;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var cultureInfo = System.Globalization.CultureInfo.GetCultureInfo("Nb");
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+        Thread.CurrentThread.CurrentCulture = cultureInfo;
+
+        var numberOfWordsInDict = FrenchNorwegianDict.Dictionary.Count;
+
+        var ja = "Ja";
+
+        while (ja.ToLower().StartsWith('j')) 
+        {
+            var wordIndex = Random.Shared.Next(numberOfWordsInDict);
+
+            var (french, norwegian) = FrenchNorwegianDict.Dictionary.ElementAt(wordIndex);
+
+            var useFrench = Random.Shared.Next() % 2 > 0;
+
+            var challenge = useFrench ? french : norwegian;
+
+            Console.WriteLine(challenge);
+            Console.WriteLine("= ");
+
+            var guess = Console.ReadLine();
+            var expected = useFrench ? norwegian : french;
+
+
+            if (string.Compare(guess, expected, true, cultureInfo) == 0)
+            {
+                Console.WriteLine("Riktig!");
+            }
+            else
+            {
+                Console.WriteLine("Feil!");
+                Console.WriteLine("Svaret er: " + expected);
+            }
+
+            Console.WriteLine("Vil du spille igjen?");
+            ja = Console.ReadLine() ?? "";
+        }
+    }
+}
